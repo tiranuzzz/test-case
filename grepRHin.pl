c1895b71d3f0b7d5f3509a7843dabda6c00e594e
Yurasov Ilya <yurasov.ilya@gmail.com>
2018-12-07 11:39:30 +0500

    fixed

--- grepRHin.pl
+++ grepRHin.pl
@@ -5,11 +5,21 @@ use warnings;
 my $pattern = $ARGV[0];
 my $catalog = $ARGV[1];
 
+unless ($catalog and $pattern ) {
+    print "Script call has the following form: perl path_to_script seeking_pattern directory\n";   
+    print "Please, enter space separated parameters: seeking_pattern directory\n";
+    chomp(my $entered_line = (<STDIN>));
+    my @params = split(/\s+/, $entered_line);
+    $pattern   = $params[0];
+    $catalog   = $params[1];
+}
+
+
 sub get_files {
     my $path = shift;      
     opendir (my $dh, $path) or die "Failed to open directory $catalog : $!";         
     while (my $subpath = readdir $dh) {
-        next if $subpath eq '.' or $subpath eq '..';
+        next if $subpath =~ m/^(\.)$|^(\.\.)$/;
         search_in_file("$path/$subpath") if -f "$path/$subpath"; 
         get_files("$path/$subpath")      if -d "$path/$subpath";
     }
